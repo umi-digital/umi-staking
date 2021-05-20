c5e15f4954327f3ac7d614dae3fa5749f4bcd655
jiangjiankang <jiankangjiang@creditease.cn>
2021-05-21 07:04:53 +0800

    add compound

--- src/contracts/Calculator.sol
+++ src/contracts/Calculator.sol
@@ -23,12 +23,19 @@ library Calculator {
         uint256 interest,
         int128 points
     ) public pure returns (int128 amount) {
+        
         int128 div = ABDKMath64x64.divu(interest, 36500); //day rate
         int128 sum = ABDKMath64x64.add(ABDKMath64x64.fromInt(1), div);
         int128 pow = ABDKMath64x64.pow(sum, n);
         int128 mul = ABDKMath64x64.mul(ABDKMath64x64.fromInt(principal), pow);
+        
         int128 point = ABDKMath64x64.mul(mul, ABDKMath64x64.fromInt(points));
         int128 res = ABDKMath64x64.toInt(point);
         return res;
     }
+
+function compound (uint principal, uint ratio, uint n)public pure returns (uint) {
+    return ABDKMath64x64.mulu (ABDKMath64x64.pow (ABDKMath64x64.add (ABDKMath64x64.fromUInt (1),ABDKMath64x64.divu (ratio, 10**18)),n),principal);
+}
+
 }
